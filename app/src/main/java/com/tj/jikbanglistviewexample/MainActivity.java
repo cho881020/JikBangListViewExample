3d4c674df1e4e383ea156e27d8270dff20f9dd54
kj_cho <kj_cho@idealidea.co.kr>
2019-04-20 14:36:15 +0900

    데이터를 ArrayList에 모두 집어넣음.

--- app/src/main/java/com/tj/jikbanglistviewexample/MainActivity.java
+++ app/src/main/java/com/tj/jikbanglistviewexample/MainActivity.java
@@ -5,8 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.tj.jikbanglistviewexample.databinding.ActivityMainBinding;
+import com.tj.jikbanglistviewexample.datas.Room;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Room> roomList = new ArrayList<>();
 
     ActivityMainBinding act;
 
@@ -17,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
         act.cleanRoomCountTxt.setText("32개");
 
+        fillRooms();
+
+    }
+
+    void fillRooms() {
+        roomList.add(new Room(500, 35, "서구 둔산동", 7, "타임월드 인근~~"));
+        roomList.add(new Room(500, 45, "동구 둔산동", 3, "갤러리백화점 부근 투룸"));
+        roomList.add(new Room(500, 35, "북구 둔산동", 20, "둔산동 아너스빌"));
+        roomList.add(new Room(500, 35, "남구 둔산동", 2, "시세대비 저렴"));
+        roomList.add(new Room(500, 40, "서구 둔산동", 15, "리치빌오피스텔 15평형"));
+        roomList.add(new Room(500, 35, "북구 둔산동", 15, "시청역부근 리치빌"));
     }
 }
