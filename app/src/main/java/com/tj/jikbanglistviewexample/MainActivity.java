cd73acd19d1a98fa775ebac2c1c50cb2fde2ee57
kj_cho <kj_cho@idealidea.co.kr>
2019-04-20 15:51:06 +0900

    리스트뷰와 어댑터를 메인액티비티에서 연결

--- app/src/main/java/com/tj/jikbanglistviewexample/MainActivity.java
+++ app/src/main/java/com/tj/jikbanglistviewexample/MainActivity.java
@@ -4,6 +4,7 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tj.jikbanglistviewexample.adapters.RoomAdapter;
 import com.tj.jikbanglistviewexample.databinding.ActivityMainBinding;
 import com.tj.jikbanglistviewexample.datas.Room;
 
@@ -11,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RoomAdapter mRoomAdapter;
 
     List<Room> roomList = new ArrayList<>();
 
@@ -24,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         act.cleanRoomCountTxt.setText("32개");
 
         fillRooms();
+
+
+        mRoomAdapter = new RoomAdapter(MainActivity.this, roomList);
+        act.roomListView.setAdapter(mRoomAdapter);
+
 
     }
 
