4e54628833f3312c35a103930dc5f02e1e8e658d
kj_cho <kj_cho@idealidea.co.kr>
2019-04-20 15:12:59 +0900

    getView 메쏘드 오버라이딩 기초작업

--- app/src/main/java/com/tj/jikbanglistviewexample/adapters/RoomAdapter.java
+++ app/src/main/java/com/tj/jikbanglistviewexample/adapters/RoomAdapter.java
@@ -2,6 +2,8 @@ package com.tj.jikbanglistviewexample.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import com.tj.jikbanglistviewexample.R;
@@ -24,4 +26,29 @@ public class RoomAdapter extends ArrayAdapter<Room> {
 
     }
 
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+
+        if (row == null) {
+            row = inf.inflate(R.layout.room_list_item, null);
+        }
+
+
+        return row;
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
