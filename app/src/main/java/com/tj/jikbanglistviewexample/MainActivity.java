1707fae2f1511f6914ac4c8e9a53e4cd8fbf1e76
kj_cho <kj_cho@idealidea.co.kr>
2019-04-20 14:03:17 +0900

    Initial commit

---
+++ app/src/main/java/com/tj/jikbanglistviewexample/MainActivity.java
@@ -0,0 +1,21 @@
+package com.tj.jikbanglistviewexample;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tj.jikbanglistviewexample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding act;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        act.cleanRoomCountTxt.setText("32개");
+
+    }
+}
